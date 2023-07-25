f69664cfaada9169c74436d2cc9fe7fc523c9929
tariq2518 <tariq.hussain2518@gmail.com>
2023-07-26 00:28:25 +0500

    Rating widget Complete

--- app/src/main/java/com/tariq/customcomposeratingbar/MainActivity.kt
+++ app/src/main/java/com/tariq/customcomposeratingbar/MainActivity.kt
@@ -3,41 +3,89 @@ package com.tariq.customcomposeratingbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.tariq.customcomposeratingbar.ui.theme.CustomComposeRatingBarTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CustomComposeRatingBarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
+                var rating by remember { mutableStateOf(0.0) }
+                var inputValue by remember { mutableStateOf("") }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    // Title
+                    Text(
+                        text = "Compose Rating Widget",
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.size(40.dp))
+
+                    // Input Field
+                    TextField(
+                        value = inputValue,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        onValueChange = {
+                            inputValue = it
+                            if (it.isEmpty()) {
+                                rating = 0.0
+                            }
+                        },
+                    )
+
+                    Spacer(modifier = Modifier.size(20.dp))
+
+                    // Change Rating Button
+                    Button(onClick = {
+                        val enteredValue = inputValue.toDoubleOrNull() ?: 0.0
+                        rating += enteredValue
+                    }) {
+                        Text("Change Rating")
+                    }
+
+
+                    Spacer(modifier = Modifier.size(20.dp))
+
+                    // Rating Widget
+                    RatingWidget(
+                        modifier = Modifier.padding(5.dp),
+                        rating = rating
+                    )
                 }
             }
+
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CustomComposeRatingBarTheme {
-        Greeting("Android")
-    }
-}
